7463f659fa95d2c80cbba9750c5d68db1a4a3055
liudekun <liudekun@taixincf.com>
2024-01-23 16:19:48 +0800

    init

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 
-export type CustomersProps = {
-  /** 名称 */
-  name: string;
-};
-
 /**
  * @component  组件名称
  * @param props { CustomersProps }
  * @returns {JSX.Element}
  */
-export default function Customers(props: CustomersProps) {
+export default function Customers() {
   return <p>Customers page</p>;
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 
-export type InvoiceProps = {
-  /** 名称 */
-  name: string;
-};
-
 /**
  * @component  组件名称
  * @param props { InvoiceProps }
  * @returns {JSX.Element}
  */
-export default function Invoice(props: InvoiceProps) {
+export default function Invoice() {
   return <p>Invoice Page</p>;
 }
